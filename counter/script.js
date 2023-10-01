c76be219ee917b581ee8ac97e059859d5dad84e4
varunharikumar <varun.harikumar@comorin.co>
2023-10-01 14:19:14 +0530

    Added counter task

---
+++ counter/script.js
@@ -0,0 +1,35 @@
+    let inputDate = document.getElementById("center")
+    console.log(inputDate);
+    setTimeout(() => {
+        inputDate.innerHTML = "10"
+        setTimeout(() => {
+            inputDate.innerHTML = "9"
+            setTimeout(() => {
+                inputDate.innerHTML = "8"
+                setTimeout(() => {
+                    inputDate.innerHTML = "7"
+                    setTimeout(() => {
+                        inputDate.innerHTML = "6"
+                        setTimeout(() => {
+                            inputDate.innerHTML = "5"
+                            setTimeout(() => {
+                                inputDate.innerHTML = "4"
+                                setTimeout(() => {
+                                    inputDate.innerHTML = "3"
+                                    setTimeout(() => {
+                                        inputDate.innerHTML = "2"
+                                        setTimeout(() => {
+                                            inputDate.innerHTML = "1"
+                                            setTimeout(() => {
+                                                inputDate.innerHTML = "Happy Independence Day"
+                                            }, 1000);
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
